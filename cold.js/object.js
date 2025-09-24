828a4ad39377a9b936cb16f29920a6ee0ce64d0f
coldwinds003 <157644169+coldwinds003@users.noreply.github.com>
2025-09-24 11:14:54 +0000

    objects

--- cold.js/object.js
+++ cold.js/object.js
@@ -1,4 +1,4 @@
-const mysym = Symbol("key1");
+/*const mysym = Symbol("key1");
 
 const jsuser = {
     name: "sidhartha",
@@ -20,3 +20,35 @@ jsuser.greeting2 = function(){
 // call the functions
 jsuser.greeting2(); // hi sidhartha sidhartha
 jsuser.greeting1(); // hi sidhartha
+*/
+const tinderuser= new Object()
+tinderuser.name="sidhartha jana"
+tinderuser.age=12
+tinderuser.email="sidharthajan13@gmail.com"
+console.log(tinderuser);
+const regularuser={
+    fullname:{
+    email:"coldwinds003@gmail.com",
+    name:"sidahrtha jana"
+    }
+
+}
+console.log(regularuser.fullname);
+const obj1={
+    1:"a",
+    2:"b",
+    3:"c",
+    4:"d"
+}
+const obj2={
+    1:"e",
+    2:"f",
+    3:"g",
+    4:"h"
+}
+const obj3=Object.assign({},obj1,obj2);
+console.log(obj3);
+console.log(Object.keys(tinderuser)); 
+console.log(Object.values(tinderuser)); 
+console.log(Object.entries(tinderuser)); 
+

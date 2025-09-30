f0d8d69b4e83dd8279279845c346c4f711794af2
coldwinds003 <157644169+coldwinds003@users.noreply.github.com>
2025-09-30 08:58:03 +0000

    new

---
+++ cold.js/hello.js
@@ -0,0 +1,31 @@
+/*console.log("hello");
+function one(){
+    const username="sidhartha"
+    function two(){
+        const work="student"
+        console.log(username);
+        console.log(work);
+    }
+    two()
+}
+one()
+*/
+
+if(true){
+    const username="sidhartha"
+    if(username=="sidhartha"){
+        const website="youtube"
+        console.log(username + website);
+    }
+    console.log(username);
+}
+
+function addone(num){
+    return num+1
+}
+console.log(addone(9));
+
+const addtwo = function(num1){
+    return num1+2
+}
+console.log(addtwo(9));
